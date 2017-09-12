9ca90fbfaa5565ac6e35d8875788a6b123ccf21c
jesus <jesus@gmail.com>
2017-09-12 09:53:57 -0600

    Added views to create/edit blog post inside the site

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 from django.utils import timezone
 
+class Persona(models.Model):
+    nombre = models.CharField(max_length=250)
+    edad = models.IntegerField()
+    class Meta:
+        verbose_name = "Persona"
+        verbose_name_plural = "Personas"
+
+    def __str__(self):
+        return self.nombre
+
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey(Persona)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
@@ -14,3 +24,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+    
+        
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import include, url
 from . import views
+from django.contrib import admin 
 urlpatterns = [
     url(r'^$', views.post_list),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+
 ]
